51f8f5bae8259e7c55cde9756db3e4b2e1e4ef00
Urban Vadzim <Herr.Wallace@protonmail.com>
2022-08-21 16:43:30 +0300

    feat: add Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { AppContainer } from './styles'
 import { Column } from './Column';
+import { Card } from './Card';
 
 export const App = () => {
   return (
     <AppContainer>
-      <Column text='To do'/>
+      <Column text='To do'>
+        <Card text='Breakfast' />
+      </Column>
       <Column text='In progress'/>
       <Column text='Done'/>
       <Column text='+ Add new'/>
--- src/styles.ts
+++ src/styles.ts
@@ -13,11 +13,21 @@ export const ColumnContainer = styled.div`
   background-color: #ebecf0;
   width: 300px;
   min-height: 40px;
-  border-radius: 3px;
+  border-radius: 5px;
   margin: 20px;
   padding: 10px;
 `
 
 export const ColumnTitle = styled.div`
   font-weight: bold;
+  padding: 5px 10px;
+`
+
+export const CardContainer = styled.div`
+  background-color: #fff;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  max-width: 280px;
+  border-radius: 5px;
+  cursor: pointer;
 `
